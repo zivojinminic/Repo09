24036867bd69470b1ed531c06ce89eb43e23205b
zivojinminic <zivojinminic05@gmail.com>
2021-09-21 15:06:18 +0200

    Else Statement

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line 
+var result;
+function functionElse(num) {
+    if (num <= 5) {
+        return result = "5 or smaller";
+    } else {
+        return result = "Bigger than 5";
+    }
+}
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+// Only change code above this line
+module.exports = functionElse;
